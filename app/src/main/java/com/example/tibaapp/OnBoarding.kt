84a9096aa751d78393cf0ba59ee7ac6c0dd8267f
AntonyIS <antonyshikubu@gmail.com>
2020-05-25 21:11:24 +0300

    Initial commit

--- app/src/main/java/com/example/tibaapp/OnBoarding.kt
+++ app/src/main/java/com/example/tibaapp/OnBoarding.kt
@@ -1,12 +1,22 @@
 package com.example.tibaapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_on_boarding.*
 
 class OnBoarding : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_on_boarding)
+
+        btn_get_started.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+//        hooks
+        val sliderAdapter = SliderAdapter(this)
+        slider.adapter = sliderAdapter
+
     }
 }
